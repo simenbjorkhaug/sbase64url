859f0ebf069182aeddf16e380e43d35bc0f1f359
Simen Bjorkhaug <simen@bjorkhaug.io>
2023-11-10 15:33:21 +0100

    fix: base64decode padding mistake

--- src/base64url.ts
+++ src/base64url.ts
@@ -2,13 +2,15 @@ export function encodeBase64url(value: Uint8Array | string): string {
   let binaryString: string
 
   if (value instanceof Uint8Array) {
-    binaryString = String.fromCharCode(...value)
+    binaryString = btoa(String.fromCharCode(...value))
   } else {
     binaryString = value
   }
 
-  const base64 = btoa(binaryString)
-  return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+  return binaryString
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '')
 }
 
 export function decodeBase64url(value: string): string {
@@ -16,14 +18,11 @@ export function decodeBase64url(value: string): string {
     .replace(/-/g, '+')
     .replace(/_/g, '/')
 
-  base64 += '=='.slice(0, (4 - (base64.length % 4)) % 4)
+  const padding = 4 - (base64.length % 4)
 
-  const binaryString = atob(base64)
-  const bytes = new Uint8Array(binaryString.length)
-
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i)
+  if (padding !== 4) {
+    base64 += '='.repeat(padding)
   }
 
-  return new TextDecoder().decode(bytes)
+  return base64
 }
